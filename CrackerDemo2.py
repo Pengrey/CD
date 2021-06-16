98a6e7851ca938e8c45149312d3b033e88190aaf
Pengrey <55480558+RFL1M4@users.noreply.github.com>
2021-06-16 17:26:02 +0100

    FIXED (?)

--- CrackerDemo2.py
+++ CrackerDemo2.py
@@ -6,12 +6,12 @@ def getPswds(charlist, increment, pswd: str = "a"):
         new_pswd = list(pswd)
         for i in range(len(new_pswd)-1,-1,-1):
             if (charlist.index(new_pswd[i]) + remain) >= limit:
-                if(i != len(new_pswd)):
+                if(i != 0):
                     new_pswd[i] = charlist[0]
                     remain = 1
                 else:
                     new_pswd[i] = charlist[0]
-                    new_pswd.insert(charlist[0])
+                    new_pswd.insert(0, charlist[0])
                     remain = 0
             else:
                 new_pswd[i] = charlist[charlist.index(new_pswd[i]) + remain]
@@ -24,9 +24,9 @@ def getPswds(charlist, increment, pswd: str = "a"):
 
 chars = string.ascii_uppercase + string.ascii_lowercase + string.digits                     # Chars used to make the password
 offset = 1                                                                                  # Number of Slaves
-pswd = "b9"                                                                                  # Previous password
+pswd = "9"                                                                                  # Previous password
 pswd = getPswds(chars, offset, pswd)                                                        # Next password to try it on
-print(pswd)
-#for i in range(0,197):
-#    pswd = getPswds(chars, offset, pswd)
-#    print(pswd)
+
+for i in range(0,197):
+    pswd = getPswds(chars, offset, pswd)
+    print(pswd)
