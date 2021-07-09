a4602e22d0ad090134d11957c567ab481e6eac6d
RFL1M4 <rodrigoflima@ua.pt>
2021-07-09 09:25:28 +0100

    Work

---
+++ Projeto/general/pedroConas/utils.py
@@ -0,0 +1,9 @@
+def dht_hash(text, seed=0, maximum=2**10):
+    """ FNV-1a Hash Function. """
+    fnv_prime = 16777619
+    offset_basis = 2166136261
+    h = offset_basis + seed
+    for char in text:
+        h = h ^ ord(char)
+        h = h * fnv_prime
+    return h % maximum
